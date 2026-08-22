1ad65d11df84ece40b6b6d58959d00b1435cb942
Leo Nistor <leo.nistor@gmail.com>
2026-08-22 17:42:01 +0300

    Add qrcode to dev server

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@rsbuild/core"
 import { pluginReact } from "@rsbuild/plugin-react"
 import { pluginTailwindcss } from "@rsbuild/plugin-tailwindcss"
+import * as QRCode from "qrcode"
 
 export default defineConfig({
   html: {
@@ -8,6 +9,22 @@ export default defineConfig({
   },
   alias: {
     "@": "./src",
+  },
+  server: {
+    host: "0.0.0.0",
+    printUrls({ urls }) {
+      urls.map((url) => console.log(`  ➜  ${url}`))
+      console.log()
+      // use only the last URL to generate the QR code
+      QRCode.toString(
+        urls.pop() ?? urls[0],
+        { type: "terminal" },
+        (err, url) => {
+          if (err) throw err
+          console.log(url)
+        }
+      )
+    },
   },
   plugins: [
     pluginReact({
