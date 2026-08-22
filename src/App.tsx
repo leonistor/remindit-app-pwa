52942eebb5e29592497d81f663bd45c39812f918
Leo Nistor <leo.nistor@gmail.com>
2026-08-22 14:06:22 +0300

    fix css import, add first shark ui component, button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 const App = () => {
   return (
     <div className="">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <Button>hello</Button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles/globals.css"
 
-const rootEl = document.getElementById("root");
+import App from "./App"
+
+const rootEl = document.getElementById("root")
 if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
+  const root = ReactDOM.createRoot(rootEl)
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
-  );
+    </React.StrictMode>
+  )
 }
