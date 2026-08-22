522a25340fe521d705179ffccf3d1ec4921ba8c9
Leo Nistor <leo.nistor@gmail.com>
2026-08-22 13:43:17 +0300

    blank

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
+import { defineConfig } from "@rsbuild/core"
+import { pluginReact } from "@rsbuild/plugin-react"
+import { pluginTailwindcss } from "@rsbuild/plugin-tailwindcss"
 
-// Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  html: {
+    template: "public/index.html",
+  },
+  alias: {
+    "@": "./src",
+  },
   plugins: [
     pluginReact({
       reactCompiler: true,
     }),
     pluginTailwindcss(),
   ],
-});
+})
--- rstest.config.ts
+++ rstest.config.ts
@@ -1,8 +1,8 @@
-import { withRsbuildConfig } from '@rstest/adapter-rsbuild';
-import { defineConfig } from '@rstest/core';
+import { withRsbuildConfig } from "@rstest/adapter-rsbuild"
+import { defineConfig } from "@rstest/core"
 
 // Docs: https://rstest.rs/config/
 export default defineConfig({
   extends: withRsbuildConfig(),
-  setupFiles: ['./tests/rstest.setup.ts'],
-});
+  setupFiles: ["./tests/rstest.setup.ts"],
+})
