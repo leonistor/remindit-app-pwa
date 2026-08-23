2e70fd4f7c1d5734e1cef1fc9ef9c787c9f3ff18
Leo Nistor <leo.nistor@gmail.com>
2026-08-23 22:35:56 +0300

    ResizablePanels ids

--- src/examples/Resizable.tsx
+++ src/examples/Resizable.tsx
@@ -9,16 +9,16 @@ const ResizableExample = () => (
     className="h-full rounded-md border"
     defaultSize={[30, 70]}
     orientation="vertical"
-    panels={[{ id: "1", minSize: 25, maxSize: 40 }, { id: "2" }]}
+    panels={[{ id: "selected", minSize: 25, maxSize: 40 }, { id: "all" }]}
   >
-    <ResizablePanel className="flex items-center justify-center" id="1">
-      Sidebar
+    <ResizablePanel className="flex items-center justify-center" id="selected">
+      Selected items
     </ResizablePanel>
 
-    <ResizableResizeTrigger id="1:2" withHandle />
+    <ResizableResizeTrigger id="selected:all" withHandle />
 
-    <ResizablePanel className="flex items-center justify-center" id="2">
-      Content
+    <ResizablePanel className="flex items-center justify-center" id="all">
+      All items
     </ResizablePanel>
   </Resizable>
 )
