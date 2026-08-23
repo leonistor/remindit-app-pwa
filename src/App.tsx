0b34e5d297137d773255284643f1a50d928efb25
Leo Nistor <leo.nistor@gmail.com>
2026-08-23 14:52:02 +0300

    theme toggle fiasco

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import { AppWindowIcon } from "@phosphor-icons/react"
 import { Separator } from "@/components/ui/separator"
-import { ModeToggle } from "./elements/theme/mode-toggle"
-import { ThemeProvider } from "./elements/theme/theme-provider"
+
 import GroupingListExample from "./examples/GroupingList"
-import TransferListExample from "./examples/TransferList"
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="remindit-ui-theme">
-      <div className="container mx-auto min-h-screen">
-        <div className="flex flex-col gap-4">
-          <div className="flex h-16 flex-row items-center gap-1">
+    <div className="container mx-auto min-h-screen">
+      <div className="flex flex-col gap-4">
+        {/*menu*/}
+        <div className="flex h-16 flex-1 flex-row items-center gap-1">
+          <span>
             <AppWindowIcon size={32} />
-            <Separator />
-            <ModeToggle />
-          </div>
-          <div className="flex-1">
-            <GroupingListExample />
-            <TransferListExample />
-          </div>
+          </span>
+          <Separator orientation="vertical" />
+          <span>RemindIt</span>
+          <Separator orientation="vertical" />
+          <span>theme</span>
+        </div>
+        {/*content*/}
+        <div className="grow">
+          <p>Content here</p>
+          <GroupingListExample />
         </div>
       </div>
-    </ThemeProvider>
+    </div>
   )
 }
 
