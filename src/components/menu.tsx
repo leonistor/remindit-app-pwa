ceaef4d1481a7a5c010bc80f824348401ce9c74c
Leo Nistor <leo.nistor@gmail.com>
2026-08-23 22:21:45 +0300

    feat: use app logo in menu and push theme toggle to the right
    Replace AppWindowIcon with the RemindIt SVG logo and align the
    theme toggle to the end of the menu bar.

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,16 +1,13 @@
-import { AppWindowIcon } from "@phosphor-icons/react"
 import { Separator } from "@/components/ui/separator"
 import { ThemeToggle } from "./theme-toggle"
 
 const Menu = () => (
   <div className="flex h-16 flex-row items-center gap-2 rounded-md border bg-accent px-4">
-    <span>
-      <AppWindowIcon size={32} />
-    </span>
+    <img alt="RemindIt logo" className="size-8" src="/remindit-icon.svg" />
     <Separator orientation="vertical" />
     <span>RemindIt</span>
     <Separator orientation="vertical" />
-    <span>
+    <span className="ml-auto">
       <ThemeToggle />
     </span>
   </div>
